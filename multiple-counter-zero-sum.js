40065cfc41a88cc9cc373803b8bcbf2c6bc7a65c
Barbara Szewczyk <comamysza@gmail.com>
2022-07-14 11:53:32 +0200

    removes unnecessary code

--- multiple-counter-zero-sum.js
+++ multiple-counter-zero-sum.js
@@ -1,34 +1,23 @@
 // Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values hat sum to zero or undefined if a pair does not exist
 
 function sumZero(arr) {
-	// define array length
-	const arrLength = arr.length;
-
-	// sum 2 outer numbers and check if they sum to 0 or not
-	for (let i = 0; i < arrLength; i++) {
+	// loop as long as the array has more than 1 number
+	while (arr.length > 1) {
 		let leftIndex = 0;
 		let rightIndex = arr.length - 1;
 		let num1 = arr[leftIndex];
 		let num2 = arr[rightIndex];
 
-		// loop as long as the array has more than 1 number
-		while (arr.length > 1) {
-			let leftIndex = 0;
-			let rightIndex = arr.length - 1;
-			let num1 = arr[leftIndex];
-			let num2 = arr[rightIndex];
+		// if outer numbers sum to 0 return array with this numbers
+		// if sum is positive it means that right number is too big - remove from array
+		// if sum is negative it means that left number is to small - remove from array
 
-			// if outer numbers sum to 0 return array with this numbers
-			// if sum is positive it means that right number is too big - remove from array
-			// if sum is negative it means that left number is to small - remove from array
-
-			if (num1 + num2 === 0) {
-				return [num1, num2];
-			} else if (num1 + num2 > 0) {
-				arr.splice(rightIndex, 1);
-			} else if (num1 + num2 < 0) {
-				arr.splice(leftIndex, 1);
-			}
+		if (num1 + num2 === 0) {
+			return [num1, num2];
+		} else if (num1 + num2 > 0) {
+			arr.splice(rightIndex, 1);
+		} else if (num1 + num2 < 0) {
+			arr.splice(leftIndex, 1);
 		}
 	}
 }
