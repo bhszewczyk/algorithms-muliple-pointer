6531aa56c2b98784da256380e0fa85c84990a6a6
Barbara Szewczyk <comamysza@gmail.com>
2022-07-15 20:39:26 +0200

    adds window slider task wording

---
+++ window-slider-my-solution.js
@@ -0,0 +1,7 @@
+// Write a function called maxSubarraySum which accepts an array of integrars and a number called n. The function should calculate the maximum sum od n consecutive elements in the array.
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
+console.log(maxSubarraySum([4, 2, 1, 6], 1));
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4));
+console.log(maxSubarraySum([], 4));
