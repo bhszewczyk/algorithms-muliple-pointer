1199e235f185b0b01c20a528ec1c416148f6cc02
Barbara Szewczyk <comamysza@gmail.com>
2022-07-19 21:19:21 +0200

    adds divide and conquer task wording

---
+++ divide-and-conquer.js
@@ -0,0 +1,5 @@
+// Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the ale passed to the function is located. If the value is not found, return -1
+
+//console.log(search([1, 2, 3, 4, 5, 6], 4)); // 3
+//console.log(search([1, 2, 3, 4, 5, 6], 6)); // 5
+//console.log(search([1, 2, 3, 4, 5, 6], 11)); // -1
